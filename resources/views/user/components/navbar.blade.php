50250458760bb3c884e02ad329602fb5b790f283
alikhoursand <ali.khoursand@gmail.com>
2025-05-13 22:15:43 +0330

    updated

--- resources/views/user/components/navbar.blade.php
+++ resources/views/user/components/navbar.blade.php
@@ -2,15 +2,16 @@
     <div class="basis-full flex justify-between items-center px-2 py-4  max-w-screen-xl mx-auto">
         <div class=" basis-1/2 lg:basis-1/4 gap-2 flex items-center">
             <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-ghost btn-circle lg:hidden">
+                <label for="my-drawer-4" class="drawer-button btn btn-ghost btn-circle lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M4 6h16M4 12h8m-8 6h16"/>
                     </svg>
-                </div>
+                </label>
             </div>
-            <a href="{{route('home')}}" class="text-primary text-2xl lg:text-3xl font-bold">{{config('app.site_name')}}</a>
+            <a href="{{route('home')}}"
+               class="text-primary text-2xl lg:text-3xl font-bold">{{config('app.site_name')}}</a>
         </div>
         <div class=" hidden lg:flex justify-center align-center basis-5/12">
             <div class=" w-full">
@@ -50,6 +51,81 @@
                           d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
                 </svg>
             </button>
+        </div>
+    </div>
+</div>
+<div class="drawer drawer-start">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle"/>
+    <div class="drawer-content">
+
+    </div>
+    <div class="drawer-side z-10">
+        <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+
+
+        <div class="bg-base-200 text-base-content min-h-full w-full relative  max-w-80 px-2 py-4">
+
+
+            <div class="flex justify-between items-center">
+                <span
+                    class="text-primary text-2xl lg:text-3xl font-bold">{{config('app.site_name')}}
+                </span>
+                <label for="my-drawer-4" class="drawer-button btn btn-ghost btn-circle ">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                         stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                    </svg>
+                </label>
+            </div>
+            <div class="divider"></div>
+
+            <div class="flex flex-col gap-y-3 mt-5">
+                <a class="bg-primary text-primary-content p-2 rounded-box flex items-center gap-x-2" href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                         stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
+                    </svg>
+
+                    <span>صفحه اصلی</span>
+                </a>
+                <a class=" p-2 rounded-box flex items-center gap-x-2" href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                         stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"/>
+                    </svg>
+
+                    <span>دسته بندی ها</span>
+                </a>
+                <a class=" p-2 rounded-box flex items-center gap-x-2" href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                         stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                    </svg>
+
+                    <span>پروفایل</span>
+                </a>
+                <a class=" p-2 rounded-box flex items-center gap-x-2" href="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                         stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+                    </svg>
+
+                    <span>دانلود های من</span>
+                </a>
+            </div>
+
+            <a href="#" class="btn btn-primary  bottom-0 btn-block w-[calc(100%-16px)] mx-auto absolute bottom-0 right-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                     stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
+                </svg>
+                <span>ورود</span>
+            </a>
         </div>
     </div>
 </div>
--- resources/views/user/pages/shop/product/details.blade.php
+++ resources/views/user/pages/shop/product/details.blade.php
@@ -3,7 +3,7 @@
     <section>
         <div class="relative  h-[calc(100vh-80px)]">
             <div
-                class="absolute h-full w-full bg-linear-65 from-neutral/0 to-neutral p-8 text-neutral-content flex items-end justify-start">
+                class="absolute h-full w-full bg-linear-120 from-neutral/0 from-33% to-neutral to-70%  p-8 text-neutral-content flex items-end justify-start">
                 <div class="flex flex-col gap-y-4">
                     <div class="text-xl font-bold"> نام فیلم</div>
                     <div>1404</div>
@@ -50,8 +50,16 @@
 
         </div>
         <div class="max-w-screen-xl mx-auto p-2 mt-8">
+
+            {{-- about --}}
             <div class="p-4 rounded-box bg-base-200">
-                <h3 class="block text-2xl font-medium">
+                <h3 class="block text-2xl font-medium flex items-center gap-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="size-7">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"/>
+                    </svg>
+
                     درباره نام فیلم
                 </h3>
 
@@ -67,9 +75,82 @@
 
                 </div>
             </div>
-            <div class="p-4 rounded-box bg-base-200"></div>
+
+            {{-- download --}}
             <div class="p-4 rounded-box mt-4 bg-base-200">
-                <h3 class="block text-2xl font-medium">
+                <h3 class="block text-2xl font-medium flex items-center gap-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="size-7">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+                    </svg>
+
+                    باکس دانلود
+                </h3>
+
+                <div class="mt-6">
+
+                    <div role="alert" class="alert alert-error alert-soft">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.75"
+                             stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"/>
+                        </svg>
+
+                        <span>برای دانلود باید ابتدا این فیلم را خریداری کنید!</span>
+                    </div>
+
+
+                    <div class="mt-3 flex flex-wrap">
+                        <div class="basis-full">لینک مستقیم:</div>
+                        <div class="basis-full md:basis-1/3 p-2">
+                            <button class="btn w-full btn-success ">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5"
+                                     stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+                                </svg>
+                                کیفیت 1080p
+                            </button>
+                        </div>
+                        <div class="basis-full md:basis-1/3 p-2">
+
+                            <button class="btn w-full btn-success ">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5"
+                                     stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+                                </svg>
+                                کیفیت 720p
+                            </button>
+                        </div>
+                        <div class="basis-full md:basis-1/3 p-2">
+                            <button class="btn w-full btn-success ">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5"
+                                     stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+                                </svg>
+                                کیفیت 480p
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- actors --}}
+            <div class="p-4 rounded-box mt-4 bg-base-200">
+                <h3 class="block text-2xl font-medium flex items-center gap-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="size-7">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"/>
+                    </svg>
+
                     بازیگران
                 </h3>
 
@@ -77,26 +158,41 @@
                     @component('user.components.categories') @endcomponent
                 </div>
             </div>
+
+            {{-- comments --}}
             <div class="p-4 rounded-box mt-4 bg-base-200">
-                <h3 class="block text-2xl font-medium">
+                <h3 class="block text-2xl font-medium flex items-center gap-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="size-7">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"/>
+                    </svg>
+
                     نظرات کاربران
                 </h3>
 
-                <div class="mt-6 grid grid-cols-3 gap-8">
-                    <div class="col-span-3 lg:col-span-1">
+                <div class="mt-6 ">
+                    <div class="">
 
 
-{{--                        <div role="alert" class="alert alert-warning alert-soft">--}}
-{{--                            <span>برای ثبت نظر ابتدا وارد شوید</span>--}}
-{{--                        </div>--}}
+                        <div role="alert" class="alert alert-warning alert-soft">
+                            <span>برای ثبت نظر باید ابتدا وارد شوید</span>
+                        </div>
 
-                        <div class="w-full ">
+                        <div class="w-full mt-3 md:w-2/3 lg:w-1/2 xl:w-2/5">
                             <textarea class="textarea w-full block mb-2" placeholder="نظر شما"></textarea>
-                            <button class="btn btn-primary">ثبت نظر</button>
+                            <button class="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="2" stroke="currentColor" class="size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5"/>
+                                </svg>
+
+                                ثبت نظر
+                            </button>
                         </div>
                     </div>
-                    <div class="divider lg:hidden col-span-3"></div>
-                    <div class="col-span-3 lg:col-span-2 divide-y">
+                    <div class="divider"></div>
+                    <div class=" lg:col-span-2 divide-y">
                         @php
                             $w = array(1,2,3,4,5,6);
                         @endphp
@@ -112,7 +208,10 @@
 
                                     <div class="opacity-75">نام کاربر</div>
                                 </div>
-                                <div class="mt-2">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</div>
+                                <div class="mt-2">
+                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                                </div>
                             </div>
                         @endforeach
                     </div>
